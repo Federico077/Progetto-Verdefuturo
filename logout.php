90ec2034b6219a11d05a5aecb1a7ed2f8538193d
Federico077 <orecchinifederico4@gmail.com>
2026-05-13 22:46:53 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-/* elimina tutte le variabili di sessione */
+
 $_SESSION = [];
 
-/* distrugge la sessione */
+
 session_destroy();
 
-/* redirect alla pagina di login */
+
 header("Location: login.php");
 exit;
